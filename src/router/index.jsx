29f32874f79b2db71418410a080d63b16707a431
unknown <bg072250@gmail.com>
2025-07-15 11:01:33 +0900

    Login Page UI

---
+++ src/router/index.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from '../features/auth/LoginPage';
+// import RegisterPage from '../features/auth/RegisterPage';
+// import DashboardPage from '../features/dashboard/DashboardPage';
+// 등 필요한 페이지 import
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      {/* 추가 경로 */}
+      {/* <Route path="/register" element={<RegisterPage />} /> */}
+      {/* <Route path="/dashboard" element={<DashboardPage />} /> */}
+    </Routes>
+  );
+};
+
+export default AppRoutes;
