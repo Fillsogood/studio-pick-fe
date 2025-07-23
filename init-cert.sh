c2fa601cce7bb044df2ab6719fc8920c4f92b1c8
Fillsogood <jdd05273@naver.com>
2025-07-23 22:24:34 +0900

    feat:ci-cd

--- init-cert.sh
+++ init-cert.sh
@@ -1,10 +1,18 @@
 #!/bin/sh
 
-if [ ! -f /etc/letsencrypt/live/studio.studiopick.p-e.kr/fullchain.pem ]; then
-  echo "📢 SSL 인증서 발급 시작"
+DOMAIN="studio.studiopick.p-e.kr"
+
+# 인증서가 없으면 발급
+if [ ! -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
+  echo "📢 인증서 없음 → 발급 시도 중..."
   certbot certonly --non-interactive --agree-tos --nginx \
-    -d studio.studiopick.p-e.kr \
+    -d $DOMAIN \
     -m your-email@example.com
-else
-  echo "✅ 이미 인증서가 존재합니다"
+fi
+
+# 인증서가 있으면 SSL 설정 복사
+if [ -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
+  echo "✅ 인증 완료 → SSL nginx.conf로 교체"
+  cp /etc/nginx/conf.d/ssl.conf.template /etc/nginx/conf.d/default.conf
+  nginx -s reload
 fi
